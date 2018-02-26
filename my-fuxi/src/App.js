e791412d05b343ff49923ae738c17f1b901567fc
李键 <1821451868@qq.com>
2018-02-26 14:37:03 +0800

    rizhi

---
+++ my-fuxi/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>fuxi</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
